b247b338b54f055d47e8103e0a7d7fb0395e3717
Michael Braun <ML.Braun@gmx.de>
2015-10-01 06:52:23 +0200

    Revert "Aptana Project File"
    This reverts commit 62b0932376dd5c4dcdb824880a47fb58212e680f.

--- administrator/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$database['HOST']    = 'localhost';
-	$database['USER']    = 'root';
-	$database['PASSWD']  = 'Mickel8-)';
-	$database['DB']      = 'simplepages';
-	$database['PRAEFIX'] = 'sp_';
-?>
